5bb9dc177c4c917d2c567c5214436e9c78a38c8a
keyler3333 <kyturk42@gmail.com>
2026-05-19 07:54:11 -0400

    Create [...nextauth].ts

---
+++ dashboard/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+
+export default NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      authorization: {
+        params: { scope: 'repo admin:repo_hook' },
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken;
+      return session;
+    },
+  },
+});
